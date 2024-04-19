fc8923723d9d527e03cc4297ff99a4f1853900cf
Paulo Lopez <geekcrasher@gmail.com>
2024-04-19 17:42:07 +0800

    refactor: imported the Home component

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,9 @@
-import { Button } from "@/components/ui/button"
+import Home from "@/components/Home"
 
 function App() {
-
   return (
-    // bg-[#f5f7ff]
     <main className="min-h-screen w-full flex flex-col items-center justify-center  bg-[#141c2f]">
-      <Button>Search</Button>
+      <Home />
     </main>
   )
 }
