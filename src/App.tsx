b8980b6145ea30369efa3bb805a0898850e57bd9
Paulo Lopez <geekcrasher@gmail.com>
2024-04-19 15:44:58 +0800

    added the configuration for shadcn-ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
+import { Button } from "@/components/ui/button"
 
 function App() {
 
   return (
     // bg-[#f5f7ff]
     <main className="min-h-screen w-full flex flex-col items-center justify-center  bg-[#141c2f]">
-
+      <Button>Search</Button>
     </main>
   )
 }
