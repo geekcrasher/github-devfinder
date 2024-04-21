740d61b6678ed7105bfa6e4fb1c30fd13de0bbe6
Paulo Lopez <geekcrasher@gmail.com>
2024-04-21 08:52:01 +0800

    refactr: wrap the section element inside the GithubUserProvider

--- src/components/Home/index.tsx
+++ src/components/Home/index.tsx
@@ -1,15 +1,15 @@
 import Navbar from "@/components/Navbar";
+import Searchbar from "@/components/Searchbar"
+import { GithubUserProvider } from "@/context/GithubUserContext";
 
 const Home = () => {
   return (
-    <section className="w-[50rem] h-[30rem] font-space-mono text-[#f5f7ff] max-md:w-full flex flex-col gap-4 border m-4 ">
-      <>
+    <GithubUserProvider>
+      <section className="w-[50rem] h-[30rem] font-space-mono text-[#f5f7ff] max-md:w-full flex flex-col gap-4 border m-4 ">
         <Navbar />
-      </>
-      <section className="border w-full h-auto">
-
+        <Searchbar />
       </section>
-    </section>
+    </GithubUserProvider>
   );
 }
 
