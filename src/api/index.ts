b3680aedeb114b043697951b8adaf79224fbc1bd
Paulo Lopez <geekcrasher@gmail.com>
2024-04-21 08:41:35 +0800

    refactor: removed the Authorization header

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,13 +1,10 @@
 import axios from "axios"
 
-const githubToken = import.meta.env.VITE_GITHUB_ACCESS_TOKEN
-
 export const githubUserData = async (username: string) => {
   try {
     const response = await axios.get(`https://api.github.com/users/${username}`, {
       headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        "Authorization": `Bearer ${githubToken}`
+        "Content-Type": "application/json; charset=utf-8"
       }
     })
 
