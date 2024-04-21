1f85779a80375e4aab2dd53b99a790b380d7a846
Paulo Lopez <geekcrasher@gmail.com>
2024-04-21 08:11:41 +0800

    feat: initialize a state that change from light to dark

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -1,16 +1,20 @@
 import { Button } from "@/components/ui/button"
-import { Sun, SunMedium } from "lucide-react"
+import { Moon, Sun, SunMedium } from "lucide-react"
+import { useState } from "react";
 
 const Navbar = () => {
+
+  const [darkMode, setDarkMode] = useState(true)
+
   return (
     <>
       <nav className="border flex items-center justify-between">
         <section>
-          <h2 className="font-bold text-2xl">devFinder</h2>
+          <h2 className="font-bold text-2xl">devfinder</h2>
         </section>
-        <Button className="font-space-mono font-medium gap-3">
-          <span className="text-[0.78rem]">LIGHT</span>
-          <SunMedium />
+        <Button className="flex items-center justify-center gap-2  font-space-mono font-medium" onClick={() => setDarkMode(!darkMode)}>
+          <span className="text-[0.8rem]">{darkMode ? "LIGHT" : "DARK"}</span>
+          {darkMode ? <SunMedium /> : <Moon />}
         </Button>
       </nav>
     </>
