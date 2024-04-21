e069267e6e32673f921a4f196826711e8d3d1e55
Paulo Lopez <geekcrasher@gmail.com>
2024-04-21 17:11:57 +0800

    style: added a padding property

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import Home from "@/components/Home"
 
 function App() {
   return (
-    <main className="min-h-screen w-full flex flex-col items-center justify-center  bg-[#141c2f]">
+    <main className="min-h-screen w-full flex flex-col items-center justify-center bg-[#141c2f] p-4">
       <Home />
     </main>
   )
