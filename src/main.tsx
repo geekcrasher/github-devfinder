195bdc026f0d46ff57fda8939556ae1262386c35
Paulo Lopez <geekcrasher@gmail.com>
2024-04-22 13:21:00 +0800

    refactor: wrap the App component inside the ThemeContextProvider for dakrmode feature

--- src/main.tsx
+++ src/main.tsx
@@ -2,9 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { ThemContextProvider } from './context/ThemContext.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <ThemContextProvider>
+      <App />
+    </ThemContextProvider>
   </React.StrictMode>,
 )
