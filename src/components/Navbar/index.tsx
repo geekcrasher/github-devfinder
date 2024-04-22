d1477a7184b6ebe33c3acb8dc6d246ae20b413f4
Paulo Lopez <geekcrasher@gmail.com>
2024-04-22 15:00:09 +0800

    style: added a credetials

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -9,9 +9,10 @@ const Navbar = () => {
   return (
     <>
       <nav className="flex items-center justify-between">
-        <>
+        <section className="text-sm">
           <h2 className="font-bold text-2xl text-slate-900 dark:text-white">devfinder</h2>
-        </>
+          <p className="text-gray-400">by <span className="capitalize font-bold">geekcrasher</span></p>
+        </section>
         <Button
           className="flex items-center justify-center gap-2 p-0 font-space-mono font-medium dark:text-[#f5f7ff] dark:bg-transparent bg-transparent"
           onClick={handleChangeTheme}
--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -13,7 +13,8 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            "group toast group-[.toaster]:bg-[#0079ff] group-[.toaster]:bg-white group-[.toaster]:text-slate-200 group-[.toaster]:border-slate-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-slate-950 dark:group-[.toaster]:text-slate-50 dark:group-[.toaster]:border-slate-800",
+            // dark:group-[.toaster]:bg-slate-950
+            "group toast group-[.toaster]:bg-[#0079ff] group-[.toaster]:bg-white group-[.toaster]:text-slate-200 group-[.toaster]:border-slate-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:text-slate-50 dark:group-[.toaster]:border-slate-800",
           description: "group-[.toast]:text-slate-500 dark:group-[.toast]:text-slate-400",
           actionButton:
             "group-[.toast]:bg-[#0062cb] group-[.toast]:text-slate-50 dark:group-[.toast]:bg-slate-50 dark:group-[.toast]:text-slate-900",
