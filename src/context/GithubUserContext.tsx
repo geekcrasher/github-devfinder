d876b2086407d2bac20e29b06d76f5606589f8d1
Paulo Lopez <geekcrasher@gmail.com>
2024-04-22 15:08:41 +0800

    fix: allows null for gitHubUser

--- src/context/GithubUserContext.tsx
+++ src/context/GithubUserContext.tsx
@@ -1,65 +1,66 @@
 import React, { createContext, useState, useEffect, useContext } from "react";
 import { githubUserData } from "@/api";
-import { toast } from "sonner"
+import { toast } from "sonner";
 
 type GitHubUserContextProviderProps = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 type GithubUser = {
-  login: string,
-  name: string,
-  avatar_url?: string,
-  bio?: string,
-  created_at: string,
-  public_repos?: number,
-  followers?: number,
-  following?: number,
-  location?: string,
-  company?: string,
-  twitter_username?: string,
-  blog?: string,
-  html_url: string,
-}
+  login: string;
+  name: string;
+  avatar_url?: string;
+  bio?: string;
+  created_at: string;
+  public_repos?: number;
+  followers?: number;
+  following?: number;
+  location?: string;
+  company?: string;
+  twitter_username?: string;
+  blog?: string;
+  html_url: string;
+};
 
 type GitHubUserContextType = {
-  gitHubUser: GithubUser,
-  setSearchUser: React.Dispatch<React.SetStateAction<string>>
-}
+  gitHubUser: GithubUser | null; // Allow null for gitHubUser
+  setSearchUser: React.Dispatch<React.SetStateAction<string>>;
+};
 
-const GitHubUserContext = createContext<GitHubUserContextType | null>(null)
+const GitHubUserContext = createContext<GitHubUserContextType | null>(null);
 
 export const useGitHubUser = () => {
-  return useContext(GitHubUserContext) as GitHubUserContextType
-}
+  return useContext(GitHubUserContext) as GitHubUserContextType;
+};
 
-export const GithubUserProvider = ({ children }: GitHubUserContextProviderProps) => {
-
-  const [gitHubUser, setGitHubUser] = useState<GithubUser | null>(null)
-  const [searchUser, setSearchUser] = useState("octocat")
+export const GithubUserProvider = ({
+  children,
+}: GitHubUserContextProviderProps) => {
+  const [gitHubUser, setGitHubUser] = useState<GithubUser | null>(null);
+  const [searchUser, setSearchUser] = useState("octocat");
 
   useEffect(() => {
     const fetchGitHubUser = async () => {
       try {
-        const response = await githubUserData(searchUser)
-        setGitHubUser(response)
+        const response = await githubUserData(searchUser);
+        setGitHubUser(response);
       } catch (error) {
-        console.log(error)
+        console.log(error);
         toast("Github user not found", {
           action: {
             label: "Try again",
             onClick: () => console.log("Try again"),
           },
-        })
+        });
       }
-    }
+    };
 
-    fetchGitHubUser()
-  }, [searchUser])
+    fetchGitHubUser();
+  }, [searchUser]);
 
   return (
     <GitHubUserContext.Provider value={{ gitHubUser, setSearchUser }}>
       {children}
     </GitHubUserContext.Provider>
   );
-}
+};
