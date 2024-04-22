1e69a44530725b7bd2bc57a3f27699d54ee09240
Paulo Lopez <geekcrasher@gmail.com>
2024-04-22 13:19:47 +0800

    refactor: imported the Toaster component from shadcn

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import Home from "@/components/Home"
+import { Toaster } from "./components/ui/sonner"
 
 function App() {
   return (
-    <main className="min-h-screen w-full flex flex-col items-center justify-center bg-[#141c2f] p-4">
+    <main className="min-h-screen w-full flex flex-col items-center justify-center bg-[#f6f8ff] dark:bg-[#141c2f] p-4">
       <Home />
+      <Toaster />
     </main>
   )
 }
