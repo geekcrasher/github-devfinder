1339f358adaee6cd2ddf5885b8c79c8a777cea8f
Paulo Lopez <geekcrasher@gmail.com>
2024-04-22 15:59:06 +0800

    style: change the color of the toast

--- src/components/ui/sonner.tsx
+++ src/components/ui/sonner.tsx
@@ -13,8 +13,7 @@ const Toaster = ({ ...props }: ToasterProps) => {
       toastOptions={{
         classNames: {
           toast:
-            // 
-            "group toast  group-[.toaster]:bg-white group-[.toaster]:text-slate-200 group-[.toaster]:border-slate-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-slate-950 dark:group-[.toaster]:text-slate-50 dark:group-[.toaster]:border-slate-800",
+            "group toast group-[.toaster]:bg-[#0079ff] group-[.toaster]:text-slate-200 group-[.toaster]:border-slate-200 group-[.toaster]:shadow-lg dark:group-[.toaster]:bg-[#0079ff] dark:group-[.toaster]:text-slate-50 dark:group-[.toaster]:border-slate-800",
           description: "group-[.toast]:text-slate-500 dark:group-[.toast]:text-slate-400",
           actionButton:
             "group-[.toast]:bg-[#0062cb] group-[.toast]:text-slate-50 dark:group-[.toast]:bg-slate-50 dark:group-[.toast]:text-slate-900",
